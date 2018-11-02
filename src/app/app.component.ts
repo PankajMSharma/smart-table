a0f45b32ada1361b8aceba8990702dc5bfa96dfd
PankajMSharma <pankajmsharma357@hotmail.com>
2018-11-02 10:29:55 +0530

    Task 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IEmployeeData, IEmployeeRecord } from './modals/employee-data';
 import { EmployeeService } from './services/employee-data.service';
-import { Observable } from 'rxjs/Observable';
-import { from } from 'rxjs/observable/from';
-import { of } from 'rxjs/observable/of';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 export const headers: Array<string> = ['Id', 'Name', 'Phone', 'City', 'Address 1', 'Address 2', 'Postal Code'];
 
@@ -13,18 +12,45 @@ export const headers: Array<string> = ['Id', 'Name', 'Phone', 'City', 'Address 1
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public empData: Observable<IEmployeeRecord[]>;
+  public allEmpRecords: IEmployeeRecord[];
+  public filteredData: IEmployeeRecord[];
   public headings: Array<string>;
-  public filterText: string;
+  public searchText: FormControl;
 
   constructor(private employeeService: EmployeeService) {}
 
   ngOnInit() {
     this.headings = headers;
+    this.fetchEmployeeData();
+
+    this.searchText = new FormControl();
+
+    this.searchText.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged())
+      .subscribe((text: string) => {
+        this.filterTable(text.toUpperCase());
+      });
+  }
+
+  private fetchEmployeeData() {
     this.employeeService.getEmployeeData().subscribe(this.takeResponse);
   }
 
   private takeResponse = (res: IEmployeeData) => {
-    this.empData = of(res.data);
+    this.allEmpRecords = res.data;
+    this.filteredData = this.allEmpRecords;
   }
+
+  // This will search name and city having Searched Text as substring.
+  private filterTable(text: string) {
+    this.filteredData = this.allEmpRecords.filter((record: IEmployeeRecord) => {
+      if (record.name.toLocaleUpperCase().match(text) || record.address.city.toLocaleUpperCase().match(text)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -15,7 +16,9 @@ import { KeysPipe } from './pipes/key-of-object';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     EmployeeService
